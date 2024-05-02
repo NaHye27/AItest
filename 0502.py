fee1070d69871aea42a9dc5d1d87c7a5203b2172
NAHye27 <skgp0207@naver.com>
2024-05-02 17:07:27 +0900

    INCLASS

---
+++ 0502.py
@@ -0,0 +1,7 @@
+from gtts import gTTS
+
+
+tts=gTTS("Hi hohohohihoho", lang='en') #초기화 
+tts.save("Hi_Hohoho.mp3") # 파일이름 tts에 save라는 기능도 가짐 
+# playsound.playsound("news_Son.mp3",True)
+# gtts 찾아보고 하기..?
